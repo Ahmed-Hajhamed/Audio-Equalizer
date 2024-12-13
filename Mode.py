a1c173fa8ad50b2e7d5a687b326808c43833dd53
zeyad-wail <zeyadalshakhy@gmail.com>
2024-12-13 20:46:24 +0200

    back

--- Mode.py
+++ Mode.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.signal import stft
+from scipy.signal import stft, spectrogram
 
 def compute_fft(signal_amplitudes, sampling_rate):
     fft_result =   np.fft.fft(signal_amplitudes)
@@ -37,28 +37,56 @@ def reconstruct_signal(modified_fft):
     reconstructed_signal = np.fft.ifft(modified_fft).real
     return reconstructed_signal
 
+# def plot_spectrogram(signal, sampling_rate, canvas):
+#     """
+#     Plot spectrogram using Matplotlib.
+#     """
+#     positive_freqs = signal[0]
+#     positive_magnitude = signal[1]
+#     # sampled_frequencies, segment_times, Sxx = stft(signal, fs= sampling_rate, nperseg=128, noverlap=64)
+#     frame_size = (len(positive_freqs) - 1) * 2  # Reconstruct the original frame size
+#     step_size = frame_size // 2  # Assuming 50% overlap
+#     num_frames = positive_magnitude.shape[0]
+#     segment_times = np.arange(num_frames) * (step_size / sampling_rate)
+
+#     Sxx_db = 20 * np.log10(positive_magnitude + 1e-10) 
+
+#     if canvas.no_label:
+#         canvas.no_label = False
+#         canvas.vmin, canvas.vmax = np.min(Sxx_db), np.max(Sxx_db)
+
+#     # print(canvas.vmin, canvas.vmax)
+
+#     canvas.axes.clear()
+#     cax = canvas.axes.pcolormesh(segment_times, positive_magnitude, Sxx_db, shading='gouraud', cmap='plasma', vmin=canvas.vmin, vmax=canvas.vmax)
+#     canvas.axes.set_xlabel("Time (s)")
+#     canvas.axes.set_ylabel("Frequency (Hz)")
+#     canvas.axes.set_title("Spectrogram")
+
+#     if not hasattr(canvas, 'colorbar') or canvas.colorbar is None:
+#         canvas.colorbar = canvas.figure.colorbar(cax, ax=canvas.axes)
+#         canvas.colorbar.set_label("Power (dB)")
+            
+#     else:
+#         canvas.colorbar.update_normal(cax)
+
+#     canvas.draw()
+
 def plot_spectrogram(signal, sampling_rate, canvas):
     """
     Plot spectrogram using Matplotlib.
     """
-    positive_freqs = signal[0]
-    positive_magnitude = signal[1]
-    # sampled_frequencies, segment_times, Sxx = stft(signal, fs= sampling_rate, nperseg=128, noverlap=64)
-    frame_size = (len(positive_freqs) - 1) * 2  # Reconstruct the original frame size
-    step_size = frame_size // 2  # Assuming 50% overlap
-    num_frames = positive_magnitude.shape[0]
-    segment_times = np.arange(num_frames) * (step_size / sampling_rate)
-
-    Sxx_db = 20 * np.log10(positive_magnitude.T + 1e-10) 
+    sampled_frequencies, segment_times, Sxx = spectrogram(signal, fs= sampling_rate, nperseg=128, noverlap=64)
+    Sxx_db = 10 * np.log10(Sxx + 1e-10) 
 
     if canvas.no_label:
         canvas.no_label = False
         canvas.vmin, canvas.vmax = np.min(Sxx_db), np.max(Sxx_db)
 
-
+    print(Sxx_db.shape)
 
     canvas.axes.clear()
-    cax = canvas.axes.pcolormesh(segment_times, positive_freqs, Sxx_db, shading='gouraud', cmap='plasma', vmin=canvas.vmin, vmax=canvas.vmax)
+    cax = canvas.axes.pcolormesh(segment_times, sampled_frequencies, Sxx_db, shading='gouraud', cmap='plasma', vmin=canvas.vmin, vmax=canvas.vmax)
     canvas.axes.set_xlabel("Time (s)")
     canvas.axes.set_ylabel("Frequency (Hz)")
     canvas.axes.set_title("Spectrogram")
@@ -66,7 +94,7 @@ def plot_spectrogram(signal, sampling_rate, canvas):
     if not hasattr(canvas, 'colorbar') or canvas.colorbar is None:
         canvas.colorbar = canvas.figure.colorbar(cax, ax=canvas.axes)
         canvas.colorbar.set_label("Power (dB)")
-            
+
     else:
         canvas.colorbar.update_normal(cax)
 
